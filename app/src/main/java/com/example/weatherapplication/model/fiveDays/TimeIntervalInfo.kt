2682b372e6aaec33032f0640c1d8f2c8c7317dda
MoroseWolf <maxfilimonenkov@yahoo.com>
2021-05-06 14:03:27 +0300

    edit some filds

--- app/src/main/java/com/example/weatherapplication/model/fiveDays/TimeIntervalInfo.kt
+++ app/src/main/java/com/example/weatherapplication/model/fiveDays/TimeIntervalInfo.kt
@@ -2,14 +2,14 @@ package com.example.weatherapplication.model.fiveDays
 
 import com.google.gson.annotations.SerializedName
 
-data class FullObject(
+data class TimeIntervalInfo(
     val dt: Int,
     val main: Main,
     val weather: List<Weather>,
     val clouds: Clouds,
     val wind: Wind,
     val visibility: Int,
-    val pop: Int,
+    val pop: Double,
     val sys: Sys,
     @SerializedName("dt_txt")
     val dtTxt: String
