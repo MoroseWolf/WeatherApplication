d63bfdf73a107e746a2cef05e8a17663b033399f
MoroseWolf <maxfilimonenkov@yahoo.com>
2021-05-06 20:42:13 +0300

    Added BottomNavigationView

--- app/src/main/java/com/example/weatherapplication/ui/screens/main/MainActivity.kt
+++ app/src/main/java/com/example/weatherapplication/ui/screens/main/MainActivity.kt
@@ -3,10 +3,15 @@ package com.example.weatherapplication.ui.screens.main
 import android.os.Bundle
 import android.text.Html
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.weatherapplication.R
 import com.example.weatherapplication.di.component.DaggerActivityComponent
 import com.example.weatherapplication.di.module.ActivityModule
 import com.example.weatherapplication.ui.screens.fiveDays.FiveDaysFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import javax.inject.Inject
 
 class MainActivity: AppCompatActivity(), MainContract.View {
@@ -17,6 +22,17 @@ class MainActivity: AppCompatActivity(), MainContract.View {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+        val navController = findNavController(R.id.nav_host_fragment)
+
+        val appBarConfiguration = AppBarConfiguration(setOf(
+            R.id.navigation_currentDay,
+            R.id.navigation_fiveDays
+        ))
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+
         injectDependency()
 
         presenter.attach(this)
